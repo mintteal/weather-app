17cfc90bc790b6ac37c819b4c2cac13820a60b17
mintteal <minttu.laesmaa@student.hamk.fi>
2023-01-28 18:47:31 +0200

    Response data parsed to daily data

--- src/pages/weather/[name].tsx
+++ src/pages/weather/[name].tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Button, Layout, Weather } from '@/components';
 
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
-
+import update from 'immutability-helper';
 let _ = require('lodash');
 
 interface ICity {
@@ -29,11 +29,68 @@ const options = [
   },
 ];
 
+interface Weather {
+  time: string;
+  weathercode: number;
+  temperature_2m_max: number;
+  temperature_2m_min: number;
+  sunrise: string;
+  sunset: string;
+  rain_sum: number;
+  showers_sum: number;
+  snowfall_sum: number;
+}
+
+interface Data {
+  [i: number]: {
+    [i: number]: string | number;
+  };
+}
+
 const City = ({ name, data }: ICity) => {
   const router = useRouter();
 
-  const [range, setRange] = useState<number>(1);
-  console.log('city:', data);
+  const parseData = () => {
+    let arr: any = [];
+
+    if (data) {
+      // Save needed data in helper variable
+      let daily: Data[] = data.daily;
+
+      let obj: Weather = {
+        time: '',
+        weathercode: 0,
+        temperature_2m_max: 0,
+        temperature_2m_min: 0,
+        sunrise: '',
+        sunset: '',
+        rain_sum: 0,
+        showers_sum: 0,
+        snowfall_sum: 0,
+      };
+
+      /* 
+        API returns data for seven days, so iterate through object entries
+        for seven times and set connected values to an object. 
+        Push every object into an array.
+        */
+
+      for (let i = 0; i < 7; i++) {
+        for (const [key, value] of Object.entries(daily)) {
+          _.set(obj, [key], [value][0][i]);
+        }
+
+        arr.push(obj);
+      }
+    }
+    return arr;
+  };
+
+  const week = useMemo(() => parseData(), []);
+
+  const returnWeather = useCallback((day: Weather, i: number) => {
+    return <Weather key={i} />;
+  }, []);
 
   if (router.isFallback) {
     return (
@@ -56,6 +113,7 @@ const City = ({ name, data }: ICity) => {
         </div>
 
         <div className='container__content'>
+          {week.map((day: Weather, i: number) => returnWeather(day, i))}
           <Weather />
           <Weather />
         </div>
@@ -78,7 +136,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const city = await options.find((option) => option.value === name);
 
   const settings =
-    '&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum&timezone=auto';
+    '&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,showers_sum,snowfall_sum&timezone=auto';
 
   const res = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${city?.lat}&longitude=${city?.lon}${settings}`
