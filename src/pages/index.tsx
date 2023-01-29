ba488b9373e8e1dd50c542859d9aae6d14992602
mintteal <minttu.laesmaa@student.hamk.fi>
2023-01-29 18:51:56 +0200

    Functional routing from cards on the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,19 +9,37 @@ export default function Home() {
           <p>Start by selecting a city</p>
         </div>
         <div className='container__content'>
-          <Link href='/weather/helsinki' passHref>
+          <Link
+            href={{
+              pathname: '/weather/helsinki/',
+              query: { country: 'Finland', lat: '60.17', lon: '24.95' },
+            }}
+            passHref
+          >
             <div id='hki-card' className='card card-img'>
               <span className='card__title'>Helsinki</span>
             </div>
           </Link>
 
-          <Link href='/weather/tampere' passHref>
+          <Link
+            href={{
+              pathname: '/weather/tampere/',
+              query: { country: 'Finland', lat: '61.5', lon: '23.8' },
+            }}
+            passHref
+          >
             <div id='tre-card' className='card card-img'>
               <span className='card__title'>Tampere</span>
             </div>
           </Link>
 
-          <Link href='/weather/turku' passHref>
+          <Link
+            href={{
+              pathname: '/weather/turku/',
+              query: { country: 'Finland', lat: '60.45', lon: '22.28' },
+            }}
+            passHref
+          >
             <div id='tku-card' className='card card-img'>
               <span className='card__title'>Turku</span>
             </div>
